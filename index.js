95d382a72eddfb54741d20925c7286dfe5391e54
Andrey Syroyezhkin <asyroyez@gmail.com>
2017-09-14 11:29:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  
+  var updatedRecipe = Object.assign({}, object, key)
 }
